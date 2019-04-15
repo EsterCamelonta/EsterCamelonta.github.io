675dd3bd60ace380104462d14027d9f38a2d242f
Ester Bergström <35452316+EsterCamelonta@users.noreply.github.com>
2019-04-15 12:56:41 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,5 @@
+(function() {
+    document.querySelector('h1').addEventListener('click', function(event) {
+        document.querySelector('.lorem-ipsum--hidden').classList.remove('lorem-ipsum--hidden');
+    });
+})();
