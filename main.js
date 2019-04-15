9e8edda1aa349b80908e5e51191638a2d9c04ecb
Ester Bergström <35452316+EsterCamelonta@users.noreply.github.com>
2019-04-15 13:38:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function() {
-    document.querySelector('h1').addEventListener('click', function(event) {
+    document.querySelector('.lorem-ipsum-button').addEventListener('click', function(event) {
         document.querySelector('.lorem-ipsum--hidden').classList.remove('lorem-ipsum--hidden');
     });
 })();
