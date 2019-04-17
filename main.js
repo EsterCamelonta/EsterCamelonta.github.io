eb37f11aa0462c505e697ac0fe82e2c57d63face
Ester Bergström <35452316+EsterCamelonta@users.noreply.github.com>
2019-04-17 08:07:04 +0200

    Add XML.

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 (function() {
+    var xmlHttpRequest = new XMLHttpRequest();
+
+    xmlHttpRequest.onreadystatechange = function() {
+        if ((this.readyState === 4) && (this.status === 200)) {
+            document.querySelector('.breakfast-menu').innerHTML = this.responseText;
+        }
+    };
+
+    xmlHttpRequest.open('GET', 'simple.xml', true);
+    xmlHttpRequest.send();
+
     document.querySelector('.lorem-ipsum-button').addEventListener('click', function(event) {
         document.querySelector('.lorem-ipsum--hidden').classList.remove('lorem-ipsum--hidden');
     });
